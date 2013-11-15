5e61207d4cdbfd1b0e78e8fa941c271edf5c662d
Marcin Rainka <marcin.rainka@gmail.com>
2013-11-15 22:24:12 +0100

    Getting the values from user.

--- FiveTimesFive.cpp
+++ FiveTimesFive.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 #include "FiveTimesFive.hpp"
+
+using namespace std;
+
+CFiveTimesFive::CFiveTimesFive() : m_oneRepMax(0.0f), m_progressFromWeekToWeek(0.0f),
+                                   m_weightAccuracy(0.0f) {}
+                                   
+void CFiveTimesFive::start() {
+    getValues();
+}                                   
+
+
+void CFiveTimesFive::getValues() {
+    cout << "Type one rep max: ";
+    cin >> m_oneRepMax;
+    cout << "Type progress weight from week to week: ";
+    cin >> m_progressFromWeekToWeek;
+    cout << "Type accuracy of result weights: ";
+    cin >> m_weightAccuracy;
+}
--- FiveTimesFive.hpp
+++ FiveTimesFive.hpp
@@ -1,5 +1,20 @@
+#ifndef FIVETIMESFIVE_HPP
+#define FIVETIMESFIVE_HPP
+
 //==================================================================================================
 class CFiveTimesFive {
+public:
+    CFiveTimesFive();
     
+    void start();
+    
+private:
+    void getValues();
+    
+    float m_oneRepMax;
+    float m_progressFromWeekToWeek;
+    float m_weightAccuracy;
 };
 //==================================================================================================
+
+#endif
